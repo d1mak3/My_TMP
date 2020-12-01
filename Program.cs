9c4433a1a03e18dc73cbde3d6912f40a7e6313a8
d1mak3 <d1mak3@yandex.ru>
2020-12-01 23:33:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace TMP_1Lab
+{
+  class Program
+  {
+    // Метод заключается в том, чтобы разделить стек на маленькие стеки по одному элементу и потом выполнять их слияние, параллельно сортируя
+    static void Sort(ref Stack _stackToSort)
+		{       
+      // Создаём два стека
+      Stack left = new Stack();
+      Stack right = new Stack();
+
+      // Заполняем эти два стека
+      int size = _stackToSort.StackSize;
+      for (int i = 0; i < size / 2; ++i)
+			{
+        left.Push(_stackToSort.Pop);        
+			}    
+
+      size = _stackToSort.StackSize;
+      for (int i = 0; i < size; ++i)
+			{
+        right.Push(_stackToSort.Pop);
+			}
+
+      // Если количество элементов в стеке больше одного, разделяем стек дальше (для того, чтобы разбить на стеки по 1 элементу)
+      if (left.StackSize > 1)
+			{        
+        Sort(ref left);
+			}
+      if (right.StackSize > 1)
+			{        
+        Sort(ref right);
+      }
+
+      // Создаём перевёрнутый изначальному стек
+      Stack ltemp = new Stack();
+      size = left.StackSize;
+      for (int i = 0; i < size; ++i)
+			{
+        ltemp.Push(left.Pop);
+			}      
+
+      Stack rtemp = new Stack();
+      size = right.StackSize;
+      for (int i = 0; i < size; ++i)
+			{
+        rtemp.Push(right.Pop);
+			}      
+
+      // Сортируем перевёрнутые стеки
+      while (ltemp.StackSize > 0 && rtemp.StackSize > 0)
+			{
+        if (ltemp.Back > rtemp.Back)
+        {
+          _stackToSort.Push(ltemp.Pop);          
+        }
+        else
+				{          
+          _stackToSort.Push(rtemp.Pop);          
+        }
+			}
+
+      while (ltemp.StackSize > 0)
+			{
+        _stackToSort.Push(ltemp.Pop);
+			}
+
+      while (rtemp.StackSize > 0)
+			{
+        _stackToSort.Push(rtemp.Pop);
+			}
+    }
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Write count of numbers:");
+      int countOfNumbers = Convert.ToInt32(Console.ReadLine());
+      Random rand = new Random();
+
+      Stack stackToSort = new Stack();
+
+      for (int i = 0; i < countOfNumbers; ++i)
+			{
+        stackToSort.Push(rand.Next(-50, 100));
+			}
+      //stackToSort.Push(6); stackToSort.Push(5); stackToSort.Push(3); stackToSort.Push(4);
+
+      Console.WriteLine("Original stack:");
+
+      Stack output = new Stack(stackToSort);
+      int size = output.StackSize;      
+      for (int i = 0; i < size; ++i)
+			{
+        Console.Write($"{output.Pop}\t");
+			}
+
+      Sort(ref stackToSort);
+
+      Console.WriteLine("Sorted stack:");
+
+      size = stackToSort.StackSize;
+      for (int i = 0; i < size; ++i)
+      {
+        Console.Write($"{stackToSort.Pop}\t");
+      }
+    }      
+  }
+}
---
+++ Stack.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMP_1Lab
+{
+  class Stack
+  {
+    private int[] stack = new int[0]; // Стек
+
+    public Stack()
+    {
+
+    }       
+
+    public Stack(Stack _oldStack) // Копируем стек в наш стек
+    {
+      int size = _oldStack.StackSize;
+      Stack temp = new Stack();
+      for (int i = 0; i < size; ++i)
+			{
+        int newNum = _oldStack.Pop;
+        temp.Push(newNum);
+			}
+
+      size = temp.StackSize;
+      for (int i = 0; i < size; ++i)
+			{
+        int newNum = temp.Pop;
+        this.Push(newNum);
+        _oldStack.Push(newNum);
+			}
+    }
+
+    public int StackSize // Размер стека
+    {
+      get
+      {
+        int size = 0;
+
+        foreach (int o in stack)
+        {          
+          ++size;
+        }      
+        
+        return size;
+      }
+    }
+
+    public void Push(int _newElement) // Добавление элемента в конец стека
+    {
+      int[] temp = new int[StackSize];
+      for (int i = 0; i < StackSize; ++i)
+        temp[i] = stack[i];
+
+      stack = new int[StackSize + 1];
+      stack[0] = _newElement;
+
+      for (int i = 1; i < StackSize; ++i)
+        stack[i] = temp[i - 1];
+    }    
+
+    public int Pop  // Вывод и удаление последнего элемента
+    {
+      get
+      {
+        int popped = stack[0];
+        int[] temp = stack;
+        stack = new int[StackSize - 1];       
+
+        int tempsize = 0;
+        foreach (int o in temp)
+          ++tempsize;
+
+        for (int i = 0; i < StackSize; ++i)
+          stack[i] = temp[i + 1];
+
+        return popped;
+      }
+    }
+
+    public int Back // Вывод последнего элемента
+    {
+      get
+      {
+        return stack[0];
+      }
+    }
+
+    public void Clear() // Очищаем стек
+    {
+      stack = new int[0];
+    }   
+  }
+}
